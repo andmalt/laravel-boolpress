d62228cdcc2bec99ad4692a354045b43810da1a7
andmalt <andrea.maltempi@gmail.com>
2021-11-15 18:26:55 +0100

    inserted seeder

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Post;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<100; $i++){
+            $newPost = new Post();
+            $newPost->title = $faker->sentence(3);
+            $newPost->author = $faker->name();
+            $newPost->post_date = $faker->dateTime();
+            $newPost->post_content = $faker->paragraph(7,true);
+            $newPost->image_url = $faker->imageUrl(640,480,'post',true,'', true);
+            $newPost->save();
+        }
+    }
+}
