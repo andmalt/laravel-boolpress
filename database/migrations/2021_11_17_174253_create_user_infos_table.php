b648c5189417454fbc97bd3bdf56aa8cc47cb6f7
andmalt <andrea.maltempi@gmail.com>
2021-11-17 20:50:08 +0100

    created user info table

---
+++ database/migrations/2021_11_17_174253_create_user_infos_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('phone',255)->nullable();
+            $table->string('country',60)->nullable();
+            $table->string('address',255)->nullable();
+            $table->timestamps();
+
+
+            $table->foreign('user_id')
+            ->references('id')
+            ->on('users')
+            ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_infos', function (Blueprint $table) {
+            $table->dropForeign('user_infos_user_id_foreign');
+        });
+
+        Schema::dropIfExists('user_infos');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         $this->call([
-            /* UsersTableSeeder::class, */
             PostsTableSeeder::class ,
             CategoriesTableSeeder::class,
         ]);
