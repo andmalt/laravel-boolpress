d8eede9c19949206edd9574473567e0ffcec0cb0
andmalt <andrea.maltempi@gmail.com>
2021-11-23 17:45:36 +0100

    partially inserted axios

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,12 +1,39 @@
 <template>
-    <div class="container">
-        <h1>Prova Prova</h1>
-    </div>
+    <Main />
 </template>
 
 <script>
+
+import Main from "./posts/Main.vue"
+import Axios from "axios";
+
 export default {
     name:'App',
+    components:{
+        Main,
+    },
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        getPostList(){
+            Axios.get('/user?ID=12345')
+            .then(function (res) {
+                // handle success
+                console.log(res);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+        }
+    }
 }
 </script>
 
---
+++ resources/js/components/posts/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+      <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-10 col-lg-8">
+
+            </div>
+        </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+    name:'Main',
+    data(){
+        return{
+
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
